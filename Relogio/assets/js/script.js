8b9dc02ff11dd090050d2bbfce982e7f5c631c17
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2023-03-17 22:03:08 -0300

    5ª Atualização Relógio Digital

--- Relogio/assets/js/script.js
+++ Relogio/assets/js/script.js
@@ -10,7 +10,7 @@ const relogio = setInterval(function time(){
 
     if (hr < 10) hr = "0" + hr;
     if (min < 10) min = "0" + min;
-    if (hr < 10) hr = "0" + hr;
+    if (s < 10) s = "0" + s;
  
     horas.textContent = hr;
     minutos.textContent = min;
